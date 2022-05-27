524e15feacc931ff4d0643d86b6efcaa0cb8194d
E'Gonzalez <ingenieria12@telemetrik.com.co>
2022-05-27 11:10:46 -0500

    modifications in aside and navbar

--- src/app/pages/dashboard/sections/sidebar/sidebar.component.ts
+++ src/app/pages/dashboard/sections/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
