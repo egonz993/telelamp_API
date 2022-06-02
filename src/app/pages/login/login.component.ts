b218329f3f726a02a6329e3cbf1a56188dd27745
E'Gonzalez <ingenieria12@telemetrik.com.co>
2022-06-02 11:41:46 -0500

    helium

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -18,63 +18,11 @@ export class LoginComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    //this.userSelector()
-    
+    this.gotoDashboard(0)
   }
 
-
-  async userSelector():Promise<any>{
-    const { value: user } = await Swal.fire({
-      title: 'Select your account',
-      input: 'select',
-      inputOptions: {
-        'telemetrik': 'Telemetrik Test',
-        'sabaneta': 'Sabaneta'
-      },
-      showCancelButton: false,
-      inputValidator: (value) => {
-        return new Promise((resolve) => {
-          if (value === 'telemetrik') {
-            resolve('')
-
-          } else if (value === 'sabaneta') {
-            resolve('')
-
-
-          } else {
-
-            resolve('Invalid Option)')
-          }
-        })
-      }
-    })
-    
-    
-
+  gotoDashboard(userID:number){
+    this.router.navigate(['/dashboard'], {queryParams: {user: userID}});
   }
-
-
-  gotoDashboard(user:string){
-    if(user === 'telemetrik'){
-      this.user = "telemetrik"
-      this.userName = "alcaldiamedellin"
-      this.userPass = "alcaldiamedellin"
-      this.userID = 0
-    }
-
-    if(user === 'sabaneta'){
-      this.user = "sabaneta"
-      this.userName = "hector.hoyos.ceballos"
-      this.userPass = "Lgf^Ha2K$Z"
-      this.userID = 1
-    }
-    
-    this.router.navigate(['/dashboard'], {queryParams: {user: this.userID}});
-  }
-
-  gotoWeb(url:string){
-
-  }
-
 
 }
